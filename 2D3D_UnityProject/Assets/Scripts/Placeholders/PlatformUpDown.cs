2efb2a7be1916edc53e87c4e48b17c0d6488aab1
Zoe <sorafnlform@gmail.com>
2019-07-09 23:12:56 -0400

    Working baby proto

---
+++ 2D3D_UnityProject/Assets/Scripts/Placeholders/PlatformUpDown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformUpDown : MonoBehaviour {
+
+	public float uplimit;
+	public float downlimit;
+	public float speed = 0.1f;
+	public float direction = 1f;
+	public float delay = 1f;
+	public bool isMoving = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void FixedUpdate() {
+		if (transform.position.y >= uplimit && isMoving) {
+			direction = -1f;
+			isMoving = false;
+			StartCoroutine ("Wait");
+		} else if (transform.position.y <= downlimit && isMoving) {
+			isMoving = false;
+			direction = 1f;
+			StartCoroutine ("Wait");
+		} else if (isMoving) {
+			transform.Translate (Vector3.up * direction * speed);
+		}
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds (delay);
+		isMoving = true;
+		transform.Translate (Vector3.up * direction * speed);
+	}
+}
