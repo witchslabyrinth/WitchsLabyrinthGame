74ee64dd1a5f731a8a55a66156d5372965d9ba3f
Drew Graham <Drew Graham>
2019-10-22 16:27:20 -0400

    started work on cat follow routine

---
+++ 2D3D_UnityProject/Assets/Scripts/Cat/FollowMoveController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMoveController : MoveController
+{
+    /// <summary>
+    /// Target to follow
+    /// </summary>
+    [SerializeField]
+    protected Transform followTarget;
+
+    protected Vector3 lastPosition;
+
+    /// <summary>
+    /// Distance to lag behind target path
+    /// </summary>
+    [SerializeField]
+    [Range(0, 5)]
+    protected float lagDistance;
+
+    public override void MoveTowards(Vector3 position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveTowards(Transform transform)
+    {
+        if(followTargetCoroutine == null) {
+            followTargetCoroutine = StartCoroutine(FollowTarget(lastPosition));
+        }
+    }
+
+    Coroutine followTargetCoroutine;
+    IEnumerator FollowTarget(Vector3 position)
+    {
+        // check how far follow target has moved from last tracked position
+        // move same distance towards that follow position
+        // if distance between target and last tracked pos >= lagDistance
+        // update lastPosition and repeat
+
+
+        yield return null;
+    }
+}
---
+++ 2D3D_UnityProject/Assets/Scripts/Cat/MoveController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveController : MonoBehaviour
+{
+    public abstract void MoveTowards(Vector3 position);
+
+    public abstract void MoveTowards(Transform transform);
+}
