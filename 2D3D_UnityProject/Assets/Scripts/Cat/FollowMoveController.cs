7d72b10f95709f60146e7e28ade32461c8587217
Drew Graham <dmg9626@gmail.com>
2019-10-27 10:53:58 -0400

    Implemented basic Cat follow behavior (#4)
    * started work on cat follow routine
    
    * implemented cat follow behavior
    - component moves object towards target, stops when within radius
    - not locked to any specific axis

---
+++ 2D3D_UnityProject/Assets/Scripts/Cat/FollowMoveController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMoveController : MoveController
+{
+    [Header("Follow Settings")]
+    /// <summary>
+    /// Target to follow
+    /// </summary>
+    [SerializeField]
+    protected GameObject followTarget;
+
+    /// <summary>
+    /// Radius of satisfaction around target - stop moving when we get this far away
+    /// </summary>
+    [SerializeField]
+    protected float stoppingDistance;
+
+    public override void MoveTowards(Vector3 position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveTowards(Transform targetTransform)
+    {
+        // Get vector towards target
+        Vector3 toTarget = targetTransform.position - transform.position;
+        float distance = toTarget.magnitude;
+
+        // Move towards target if outside stopping radius
+        if (distance >= stoppingDistance) {
+            float step = moveSpeed * Time.fixedDeltaTime;
+            Vector3 movement = toTarget.normalized * step;
+
+            transform.position += movement;
+        }
+    }
+
+    // TODO: consider moving this call to an abstract behavior component (although that's most likely overkill)
+    void Update()
+    {
+        MoveTowards(followTarget.transform);
+    }
+}
---
+++ 2D3D_UnityProject/Assets/Scripts/Cat/MoveController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveController : MonoBehaviour
+{
+    /// <summary>
+    /// Movement speed
+    /// </summary>
+    [SerializeField]
+    protected float moveSpeed;
+
+    public abstract void MoveTowards(Vector3 position);
+
+    public abstract void MoveTowards(Transform transform);
+}
