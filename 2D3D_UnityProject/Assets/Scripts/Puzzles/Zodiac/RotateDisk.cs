8010bb835e137de6c1484c372745ef94eea1324e
Drew Graham <dmg9626@gmail.com>
2019-10-31 18:06:34 -0400

    generating sprites around ring! (rotations need to be fixed though...)

--- 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/RotateDisk.cs
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/RotateDisk.cs
@@ -14,7 +14,9 @@ public class RotateDisk : MonoBehaviour
     /// <summary>
     /// List (ordered containing references to symbols on the disk
     /// </summary>
-    public List<Texture2D> symbols;
+    public List<Sprite> symbols;
+
+    public SpriteRenderer spritePrefab;
 
     // TODO: do this programmatically
     /// <summary>
@@ -42,6 +44,11 @@ public class RotateDisk : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        //spritePivot.transform.RotateAround(transform.position, Vector3.forward, angleBetweenSymbols * Time.deltaTime);
+    }
+
     public void Init(ZodiacPuzzle puzzle)
     {
         // Break if no symbols found
@@ -60,15 +67,22 @@ public class RotateDisk : MonoBehaviour
 
     private void PopulateSymbols()
     {
-        Debug.LogFormat("{0}: Populating {1} symbols", name, symbols.Count);
-        // TODO: Populate disk with symbols radially separated by that angle
+        //Debug.LogFormat("{0}: Populating {1} symbols", name, symbols.Count);
         // Get radius from center to sprite pivot
         Vector3 pivotVector = spritePivot.transform.position - puzzle.center.transform.position;
-        Debug.LogFormat("Sprite distance: {0}", pivotVector.magnitude);
-        Debug.LogFormat("Pivot Vector: {0}", pivotVector);
 
-        foreach(Texture2D texture in symbols) {
+        // Instantiate each symbol around the ring
+        foreach(Sprite symbol in symbols) {
+            // Instantiate symbol at pivot
+            Debug.LogFormat("Instantiating {0}", symbol.name);
 
+            //Sprite instance = Instantiate(symbol, spritePivot.transform.position, spritePivot.transform.rotation, transform);
+            Quaternion rotation = spritePrefab.transform.rotation * spritePivot.transform.rotation;
+            SpriteRenderer instance = Instantiate(spritePrefab, spritePivot.transform.position, rotation, transform);
+            instance.sprite = symbol;
+
+            // Rotate pivot to position of next symbol
+            spritePivot.transform.RotateAround(transform.position, Vector3.forward, angleBetweenSymbols);
         }
     }
 
