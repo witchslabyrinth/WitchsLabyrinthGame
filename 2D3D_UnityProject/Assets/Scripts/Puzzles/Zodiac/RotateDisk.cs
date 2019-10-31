84bc38667e43c57b177f1b5b36c06f9ec4d11e62
Drew Graham <dmg9626@gmail.com>
2019-10-31 18:39:05 -0400

    fixed rotation when generating symbols; plugged in all symbols into each disk to see how it feels

--- 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/RotateDisk.cs
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/RotateDisk.cs
@@ -68,20 +68,19 @@ public class RotateDisk : MonoBehaviour
     private void PopulateSymbols()
     {
         //Debug.LogFormat("{0}: Populating {1} symbols", name, symbols.Count);
-        // Get radius from center to sprite pivot
-        Vector3 pivotVector = spritePivot.transform.position - puzzle.center.transform.position;
-
         // Instantiate each symbol around the ring
-        foreach(Sprite symbol in symbols) {
-            // Instantiate symbol at pivot
-            Debug.LogFormat("Instantiating {0}", symbol.name);
+        for(int i = 0; i < symbols.Count; i++) {
+            Sprite symbol = symbols[i];
 
-            //Sprite instance = Instantiate(symbol, spritePivot.transform.position, spritePivot.transform.rotation, transform);
-            Quaternion rotation = spritePrefab.transform.rotation * spritePivot.transform.rotation;
+            // Calculate sprite rotation
+            Quaternion rotation = Quaternion.Euler(0, 0, i * angleBetweenSymbols);
+
+            // Instantiate symbol at pivot point
             SpriteRenderer instance = Instantiate(spritePrefab, spritePivot.transform.position, rotation, transform);
             instance.sprite = symbol;
+            instance.name = symbol.name;
 
-            // Rotate pivot to position of next symbol
+            // Rotate pivot around ring to position of next symbol
             spritePivot.transform.RotateAround(transform.position, Vector3.forward, angleBetweenSymbols);
         }
     }
