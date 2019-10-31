7c80568acb62d86180f90b4adbf20b55d2438fd1
Drew Graham <dmg9626@gmail.com>
2019-10-31 15:53:18 -0400

    ZodiacPuzzle.cs script sends Rotate() calls to RotateDisk

--- 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/RotateDisk.cs
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/RotateDisk.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class RotateDisk : MonoBehaviour
 {
-    public enum Direction { LEFT, RIGHT }
 
     /// <summary>
     /// How fast the disk should rotate
@@ -31,14 +30,51 @@ public class RotateDisk : MonoBehaviour
     /// Holds angle (in degrees) between each symbol on the disk
     /// </summary>
     private float angleBetweenSymbols;
-    
+
     private void Start()
+    {
+
+    }
+
+    public void Init()
     {
         // Calculate angle between symbols
         int numSymbols = symbols.Count;
         angleBetweenSymbols = 360f / numSymbols;
-        Debug.LogFormat("Angle between {0} symbols: {1}", numSymbols, angleBetweenSymbols);
+        //Debug.LogFormat("Angle between {0} symbols: {1}", numSymbols, angleBetweenSymbols);
 
         // TODO: Populate disk with symbols radially separated by that angle
+        // Get radius from center to ring
+    }
+
+    /// <summary>
+    /// Rotates disk left/right towards the next symbol along the ring
+    /// </summary>
+    /// <param name="direction"></param>
+    public void Rotate(ZodiacPuzzle.Direction direction)
+    {
+        if(rotateCoroutineInstance == null) {
+            rotateCoroutineInstance = RotateCoroutine(direction);
+            StartCoroutine(rotateCoroutineInstance);
+        }
+    }
+
+    private IEnumerator rotateCoroutineInstance;
+    private IEnumerator RotateCoroutine(ZodiacPuzzle.Direction direction)
+    {
+        // Calculate degrees of rotation (in given distance)
+        float currentRotation = transform.rotation.x;
+        float change = (angleBetweenSymbols * (int)direction);
+        Debug.LogFormat("Rotating {0} degrees to the {1}", change, direction);
+
+        // Create quaternion containing change in rotation (relative to current reference frame)
+        Quaternion rotationChange = Quaternion.Euler(new Vector3(change, 0));
+        Debug.Log(rotationChange.eulerAngles);
+
+        // Rotate towards that quaternion
+
+
+        rotateCoroutineInstance = null;
+        yield return null;
     }
 }
---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/ZodiacPuzzle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZodiacPuzzle : MonoBehaviour
+{
+    public enum Direction {
+        LEFT = -1,
+        RIGHT = 1
+    }
+
+    /// <summary>
+    /// Ordered list of rotating disks, from outermost to innermost
+    /// </summary>
+    public List<RotateDisk> disks;
+
+    private RotateDisk currentDisk;
+
+    void Start()
+    {
+        // Initialize each disk
+        foreach(RotateDisk disk in disks) {
+            disk.Init();
+        }
+
+        currentDisk = disks[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            currentDisk.Rotate(Direction.LEFT);
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            currentDisk.Rotate(Direction.RIGHT);
+        }
+    }
+}
