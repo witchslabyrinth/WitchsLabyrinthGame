e22eb8ee6abbd46e5ee59b748400d2cc95a19cee
Wills Martin <wmm38@drexel.edu>
2019-11-10 13:44:08 -0500

    Zodiac tweaks (#19)
    * Zodiac disks now pop out when they are selected.
    
    * Renamed diskrotate to zodiac disk and added a parent class zodiacpuzzlepiece for it to inherit from. Also created zodiac center that pops out when the puzzle is completed.
    
    * Fixed a bug where you could 'swap disk' to a disk that wasn't there.
    
    * Comments
    
    * Made the inOutTime field serialized
    
    * Replaced PieceIn and PieceOut with one method PieceInOut.
    
    * Added enum for zodiac puzzle piece positions and changed parameter of PieceInOut to be of that type.

--- 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/ZodiacPuzzlePiece.cs
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/ZodiacPuzzlePiece.cs
@@ -13,6 +13,7 @@ public class ZodiacPuzzlePiece : MonoBehaviour
     /// <summary>
     /// Time it takes to complete the coroutines PieceOut() and PieceIn()
     /// </summary>
+    [SerializeField]
     private float inOutTime = 1.0f;
 
     /// <summary>
@@ -33,21 +34,31 @@ public class ZodiacPuzzlePiece : MonoBehaviour
     }
 
     /// <summary>
-    /// Animates a piece into the outPosition. Stops and PieceIn() that might be running.
+    /// Animates a piece into the target puzzle piece position
     /// </summary>
-    public void PieceOut()
+    public void PieceInOut(ZodiacPuzzlePiecePosition targetPosition)
     {
-        if (pieceInRoutineInstance != null)
-            StopCoroutine(pieceInRoutineInstance);
-        pieceOutRoutineInstance = PieceOutRoutine();
-        StartCoroutine(pieceOutRoutineInstance);
+        // Stop any running coroutine
+        if (pieceInOutRoutineInstance != null)
+            StopCoroutine(pieceInOutRoutineInstance);
+
+        // Set coroutine instance based on parameter
+        if (targetPosition == ZodiacPuzzlePiecePosition.In)
+            pieceInOutRoutineInstance = PieceInOutCoroutine(inPosition);
+        else if (targetPosition == ZodiacPuzzlePiecePosition.Out)
+            pieceInOutRoutineInstance = PieceInOutCoroutine(outPosition);
+        else
+            throw new System.Exception("Reached branch that shouldn't be possible.");
+
+        // Run coroutine
+        StartCoroutine(pieceInOutRoutineInstance);
     }
 
-    private IEnumerator pieceOutRoutineInstance;
-    private IEnumerator PieceOutRoutine()
+    private IEnumerator pieceInOutRoutineInstance;
+    private IEnumerator PieceInOutCoroutine(Vector3 targetPosition)
     {
         // Make an animation curve starting from current position and ending at the outPosition y values
-        AnimationCurve animationCurve = AnimationCurve.EaseInOut(0, transform.localPosition.y, inOutTime, outPosition.y);
+        AnimationCurve animationCurve = AnimationCurve.EaseInOut(0, transform.localPosition.y, inOutTime, targetPosition.y);
 
         for (float timer = 0f; timer < inOutTime; timer += Time.deltaTime)
         {
@@ -59,38 +70,13 @@ public class ZodiacPuzzlePiece : MonoBehaviour
             yield return null;
         }
 
-        // Jump to outPosition
-        transform.localPosition = outPosition;
+        // Jump to targetPosition
+        transform.localPosition = targetPosition;
     }
 
-    /// <summary>
-    /// Animates a piece into the inPosition. Stops and PieceOut() that might be running.
-    /// </summary>
-    public void PieceIn()
+    public enum ZodiacPuzzlePiecePosition
     {
-        if (pieceOutRoutineInstance != null)
-            StopCoroutine(pieceOutRoutineInstance);
-        pieceInRoutineInstance = PieceInCoroutine();
-        StartCoroutine(pieceInRoutineInstance);
-    }
-
-    private IEnumerator pieceInRoutineInstance;
-    private IEnumerator PieceInCoroutine()
-    {
-        // Make an animation curve starting from current position and ending at the outPosition y values
-        AnimationCurve animationCurve = AnimationCurve.EaseInOut(0, transform.localPosition.y, inOutTime, inPosition.y);
-
-        for (float timer = 0f; timer < inOutTime; timer += Time.deltaTime)
-        {
-            // Evaluate the animation curve at the current timer value and set the current local position y to that value
-            float animValue = animationCurve.Evaluate(timer);
-            Vector3 newPosition = transform.localPosition;
-            newPosition.y = animValue;
-            transform.localPosition = newPosition;
-            yield return null;
-        }
-
-        // Jump to inPosition
-        transform.localPosition = inPosition;
+        In = 0,
+        Out = 1
     }
 }
