ed1b6b7e1c8985779c89f2e672dd0bf6b3205a91
Victor Liu <vsl27@drexel.edu>
2019-11-11 15:43:15 -0500

    Start of LiarStatue script

---
+++ 2D3D_UnityProject/Assets/Puzzles/Liars/LiarStatue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiarStatue : MonoBehaviour
+{
+    [SerializeField]
+    private string statement;
+
+    [SerializeField]
+    private bool hasOrb;
+
+    private bool inZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inZone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //talk to this statue
+        }
+        if (Input.GetKeyDown("enter"))
+        {
+            //try to take orb from statue
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        inZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inZone = false;
+    }
+}
