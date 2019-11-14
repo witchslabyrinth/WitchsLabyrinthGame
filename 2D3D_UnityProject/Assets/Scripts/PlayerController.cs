e6a1bbe3613ea9ac3543c33c0d1b64e3ae0d3431
Victor Liu <vsl27@drexel.edu>
2019-11-14 17:31:43 -0500

    Changed F key to E key

--- 2D3D_UnityProject/Assets/Scripts/PlayerController.cs
+++ 2D3D_UnityProject/Assets/Scripts/PlayerController.cs
@@ -118,7 +118,7 @@ public class PlayerController : MonoBehaviour
             if (inDialogueZone)
                 LiarGameManager.Instance().StartConversation(dialoguePartner);
         }
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             if (inDialogueZone)
             {
