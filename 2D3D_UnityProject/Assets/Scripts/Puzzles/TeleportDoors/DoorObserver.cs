d51a7a463d6123839ce8967984ece1ec96be5b45
n8sthegr8est <naa73@drexel.edu>
2019-11-14 21:16:59 -0500

    Finished door programming & puzzle layout

---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/TeleportDoors/DoorObserver.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorObserver : MonoBehaviour
+{
+    private List<TeleDoorScript> toObserve = new List<TeleDoorScript>();
+    
+
+    public void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            if (child.GetComponent<TeleDoorScript>())
+            {
+                toObserve.Add(child.GetComponent<TeleDoorScript>());
+            }
+        }
+        for(int i = 0; i < toObserve.Count; i++)
+        {
+            toObserve[i].addDoorObserver(this);
+        }
+    }
+
+    public void doorObserve(TeleDoorScript.DoorOrientation signalFrom)
+    {
+        /*Vector3 signalFromRotation = signalFrom.localRotation.eulerAngles;
+        Vector3 newRotation = new Vector3(0,0,0);
+        if(signalFromRotation.x == 90)
+        {
+            if(signalFromRotation.y == 90)
+            {
+                newRotation = new Vector3(0, 0, 90);
+            }
+        }*/
+        Vector3 newRotation = new Vector3(0, 0, 0);
+        /*if(signalFrom.localPosition.y == 22.5)
+        {
+            newRotation = new Vector3(0, 0, 180);
+        }
+        else if (signalFrom.localPosition.x == 22.5)
+        {
+            newRotation = new Vector3(0, 0, -90);
+        }
+        else if (signalFrom.localPosition.x == -22.5)
+        {
+            newRotation = new Vector3(0, 0, 90);
+        }
+        else if(signalFrom.localPosition.z == 22.5)
+        {
+            newRotation = new Vector3(90, 0, 0);
+        }
+        else if (signalFrom.localPosition.z == -22.5)
+        {
+            newRotation = new Vector3(-90, 0, 0);
+        }*/
+        if (signalFrom == TeleDoorScript.DoorOrientation.CEILING)
+        {
+            newRotation = new Vector3(0, 0, 180);
+        }
+        else if (signalFrom == TeleDoorScript.DoorOrientation.WALL_POS_X)
+        {
+            newRotation = new Vector3(0, 0, -90);
+        }
+        else if (signalFrom == TeleDoorScript.DoorOrientation.WALL_NEG_X)
+        {
+            newRotation = new Vector3(0, 0, 90);
+        }
+        else if (signalFrom == TeleDoorScript.DoorOrientation.WALL_POS_Z)
+        {
+            newRotation = new Vector3(90, 0, 0);
+        }
+        else if (signalFrom == TeleDoorScript.DoorOrientation.WALL_NEG_Z)
+        {
+            newRotation = new Vector3(-90, 0, 0);
+        }
+        //else DoorOrientation is FLOOR, so newRotation = new Vector3(0, 0, 0);
+
+        transform.parent.transform.rotation = Quaternion.Euler(newRotation);
+    }
+}
---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/TeleportDoors/TeleDoorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleDoorScript : MonoBehaviour
+{
+    public enum DoorOrientation { FLOOR, CEILING, WALL_NEG_X, WALL_POS_X, WALL_NEG_Z, WALL_POS_Z };
+
+    [SerializeField]
+    private TeleDoorScript PairedDoor;
+    [SerializeField]
+    private Vector3 TeleportOnExitToCoordinate;
+
+    [SerializeField]
+    private DoorOrientation myOrientation = DoorOrientation.FLOOR;
+
+    private List<DoorObserver> observers = new List<DoorObserver>();
+
+    public void Enter(GameObject toSendThrough)
+    {
+        PairedDoor.Exit(toSendThrough);
+    }
+
+    public void Exit(GameObject thatWasSentThrough)
+    {
+        thatWasSentThrough.transform.position = new Vector3(TeleportOnExitToCoordinate.x,TeleportOnExitToCoordinate.y,TeleportOnExitToCoordinate.z);
+        if(observers.Count > 0)
+        {
+            for(int i = 0; i < observers.Count; i++)
+            {
+                observers[i].doorObserve(myOrientation);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enter(other.gameObject);
+    }
+
+    public void addDoorObserver(DoorObserver toAdd)
+    {
+        observers.Add(toAdd);
+    }
+}
