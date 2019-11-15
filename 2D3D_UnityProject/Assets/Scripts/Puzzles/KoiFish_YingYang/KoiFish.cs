b0ecf1844fff0fe7955a9be0de743a85600a0f41
Drew Graham <dmg9626@gmail.com>
2019-11-14 20:26:39 -0500

    got the koi fish to perform their respective correct/incorrect animations on keypress: 1 -> fish1, 2 -> fish2, 3 -> fish3 (hold shift to trigger 'wrong' animation)

---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/KoiFish_YingYang/KoiFish.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoiFish : MonoBehaviour
+{
+    /// <summary>
+    /// Order in which this fish should be activated to solve the puzzle
+    /// </summary>
+    [Range(1, 3)]
+    public int fishNumber;
+
+    private Animator animator;
+
+    private Dictionary<int, string> koiFishAnimationMap = new Dictionary<int, string>() {
+        { 1, "Koi1_Correct" },
+        { 2, "Koi2_Correct" },
+        { 3, "Koi3_Correct" },
+
+
+        { -1, "Koi1_Wrong" },
+        { -2, "Koi2_Wrong" },
+        { -3, "Koi3_Wrong" },
+    };
+
+    private Dictionary<int, KeyCode> koiFishButtonMap = new Dictionary<int, KeyCode>() {
+        { 1, KeyCode.Alpha1 },
+        { 2, KeyCode.Alpha2 },
+        { 3, KeyCode.Alpha3 }
+    };
+
+    void Start()
+    {
+        // Pass fish number to animator (used for selecting proper animations)
+        animator = GetComponentInChildren<Animator>();
+        animator.SetInteger("FishNumber", fishNumber);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(koiFishButtonMap[fishNumber])) {
+            PlayAnimation(fishNumber);
+        }
+    }
+
+
+    void PlayAnimation(int animNumber)
+    {
+        // Play wrong animation if we're holding shift
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            animNumber *= -1;
+        }
+
+
+        string animationName = koiFishAnimationMap[animNumber];
+        Debug.Log(name + " | Triggering " + animationName);
+
+        // Fire path trigger
+        animator.SetInteger("ActivationOrder", animNumber);
+        animator.SetTrigger("PathTrigger");
+    }
+}
