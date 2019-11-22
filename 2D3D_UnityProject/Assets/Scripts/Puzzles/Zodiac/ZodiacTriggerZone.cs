8501412eaa5dc905843d2305a58915a42fb3eed7
Victor Liu <vsl27@drexel.edu>
2019-11-22 03:37:54 -0500

    Code for a trigger zone to activate the zodiac puzzle

---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Zodiac/ZodiacTriggerZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZodiacTriggerZone : MonoBehaviour
+{
+    [SerializeField]
+    private ZodiacPuzzle puzzleScript;
+
+    [SerializeField]
+    private GameObject camZodiac;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+            controller.SetInZodiacZone(true, puzzleScript, camZodiac);
+        Debug.Log("In range of Zodiac Puzzle");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+            controller.SetInZodiacZone(false, puzzleScript, camZodiac);
+        Debug.Log("Out of range of Zodiac Puzzle");
+    }
+}
