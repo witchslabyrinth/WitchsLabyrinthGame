d494a74f771362a9f23f356bfdc77c30aaed361f
Drew Graham <Drew Graham>
2019-12-01 17:08:51 -0500

    FollowMovement now has followPlayer setting - sets target to Player if true (useful when not calling constructor)

--- 2D3D_UnityProject/Assets/Scripts/Player/Movement/FollowMovement.cs
+++ 2D3D_UnityProject/Assets/Scripts/Player/Movement/FollowMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(menuName="Movement/Follow Movement")]
 public class FollowMovement : Movement
 {
+    /// <summary>
+    /// Target to follow
+    /// </summary>
     private Transform target;
+
+    /// <summary>
+    /// Sets player actor as target if true
+    /// </summary>
+    [SerializeField]
+    private bool followPlayer;
 
     /// <summary>
     /// Smaller radius of satisfaction around target - stop moving when we are this close
     /// </summary>
     [SerializeField]
-    protected float stoppingDistance;
+    protected float stoppingDistance = 1;
     
     /// <summary>
     /// Larger radius around target - start following the target when we're this far away
     /// </summary>
     [SerializeField]
-    protected float startingDistance;
+    protected float startingDistance = 3;
 
     public FollowMovement(Transform target, float stoppingDistance, float startingDistance)
     {
@@ -27,8 +36,26 @@ public class FollowMovement : Movement
         // TODO: make the Get() method use starting distance again
         this.startingDistance = startingDistance;
     }
+
+    public FollowMovement(Transform target)
+    {
+        this.target = target;
+    }
+
     public override Vector3 Get(Transform player)
     {
+        if(target == null) {
+            // Set target to player if specified
+            if(followPlayer) {
+                target = PlayerController.Instance.GetActor().transform;
+            }
+            // Otherwise return 0
+            else {
+                Debug.LogWarning(player.name + " | FollowMovement.target is null, cannot generate movement");
+                return Vector3.zero;
+            }
+        }
+
         // Get vector towards target
         Vector3 toTarget = target.position - player.transform.position;
         float distance = toTarget.magnitude;
