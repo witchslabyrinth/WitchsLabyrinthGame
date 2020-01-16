d9cb55ffa0e7380a6124a2e878ae9984be2e0d7d
n8sthegr8est <naa73@drexel.edu>
2020-01-16 12:57:05 -0500

    got reflecting working

---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/Reflection/ReflectableLightNode.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectableLightNode : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject toGenerateOnReflect;
+
+    //ReflectableLightHead lightHead;
+
+    GameObject immediateChild = null;
+
+    RaycastHit rcHit;
+    Vector3 HitPosit = Vector3.zero;
+    Vector3 PrevHitPosit = Vector3.zero;
+    bool setPrevHitPosit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lightHead = GetComponent<ReflectableLightHead>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out rcHit, 10))
+        {
+            //Debug.DrawRay(transform.position, transform.forward * 10, Color.yellow);
+
+            HitPosit = rcHit.point;
+            if (!setPrevHitPosit)
+            {
+                PrevHitPosit = HitPosit;
+                setPrevHitPosit = true;
+            }
+            else if (PrevHitPosit != HitPosit)
+            {
+                immediateChild.GetComponent<ReflectableLightNode>().RemoveThisReflection();
+                immediateChild = null;
+                PrevHitPosit = HitPosit;
+            }
+            if (immediateChild == null)
+            {
+                immediateChild = Instantiate(toGenerateOnReflect, HitPosit, Quaternion.LookRotation(Vector3.Reflect(transform.forward, rcHit.normal)));//Quaternion.Euler(Vector3.Reflect(transform.forward, rcHit.normal)));
+            }
+            //Debug.Log("OBJ Detected!");
+        }
+        else
+        {
+            if (immediateChild != null)
+            {
+                PrevHitPosit = Vector3.zero;
+                immediateChild.GetComponent<ReflectableLightNode>().RemoveThisReflection();
+                immediateChild = null;
+                setPrevHitPosit = false;
+            }
+        }
+    }
+
+    public void RemoveThisReflection()
+    {
+        if (immediateChild != null)
+        {
+            immediateChild.GetComponent<ReflectableLightNode>().RemoveThisReflection();
+        }
+        Destroy(gameObject);
+    }
+}
