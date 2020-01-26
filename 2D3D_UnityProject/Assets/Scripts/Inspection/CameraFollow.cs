263a048dce65b512b6394c35de41a48ee20c30d7
Victor Liu <vsl27@drexel.edu>
2020-01-26 06:39:22 -0500

    Getting inspection into main scene

---
+++ 2D3D_UnityProject/Assets/Scripts/Inspection/CameraFollow.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//allows the camera to turn towards an interactable object that was clicked and set a 'zoomed in' flag
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public int inspectMode = 0; // 0 is not in inspect mode, 1 is in inspect mode
+
+    /// <summary>
+    /// ID of object currently being inspected
+    /// -1 when no object selected
+    /// </summary>
+    public int objectIndex = -1;
+
+    /// <summary>
+    /// Store the position of the camera before inspecting
+    /// </summary>
+    private Vector3 startCamPos;
+
+    /// <summary>
+    /// Store the rotation of the camera before inspecting
+    /// </summary>
+    private Quaternion startCamRot;
+
+    /// <summary>
+    /// store the positino of the inspectable object before inspecting
+    /// </summary>
+    private Vector3 startObjPos;
+
+    /// <summary>
+    /// stores the rotaion of the inspectable object before inspecting
+    /// </summary>
+    private Quaternion startObjRot;
+
+
+    Quaternion rotTarget;
+    public RaycastHit hit;
+
+    private GameObject inspectObj;
+
+    int cameraSpeed = 30; //use this to control camera move speed
+
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        if (Input.GetMouseButton(0) & inspectMode == 0) //check for left click
+        {
+            if (Physics.Raycast(GetComponent<Camera>().ScreenPointToRay(Input.mousePosition), out hit, 10000.0f)) //check if left mouse clicked on an object
+            {
+                inspectObj = hit.collider.gameObject;
+
+                if (inspectObj.GetComponent<PreviewObjectFunctionality>() != null)
+                {
+                    inspectMode = 1; //change to 1 (enter inspect mode)
+                    objectIndex = inspectObj.GetComponent<PreviewObjectFunctionality>().index;
+
+                    startObjPos = inspectObj.transform.position;
+                    startObjRot = inspectObj.transform.rotation;
+
+                    startCamPos = transform.position;
+                    startCamRot = transform.rotation;
+
+                    var FollowPos = hit.transform; //what to rotate to if raycast hit an object
+
+                    rotTarget = Quaternion.LookRotation(FollowPos.position - transform.position); //set up the smooth interval that Quaternion.RotateTowards uses later as an endpoint 
+                }
+            }
+        }
+
+        if (inspectMode == 1)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotTarget, Time.deltaTime * cameraSpeed); //point to rotate to at each update/frame
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) & inspectMode == 1)
+        {
+            ResetCamAndObj();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Actor player = PlayerController.Instance.GetPlayer();
+            player.Enable();
+
+            if(inspectMode == 1)
+            {
+                ResetCamAndObj();
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void ResetCamAndObj()
+    {
+        inspectMode = 0;
+        objectIndex = -1;
+        transform.position = startCamPos;
+        transform.rotation = startCamRot;
+        inspectObj.transform.position = startObjPos;
+        inspectObj.transform.rotation = startObjRot;
+    }
+}
---
+++ 2D3D_UnityProject/Assets/Scripts/Inspection/PreviewObjectFunctionality.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//allows an interactable object to be rotated in 3 dimensions
+public class PreviewObjectFunctionality : MonoBehaviour
+{
+    Vector3 mPrevPos = Vector3.zero;
+    Vector3 mPosDelta = Vector3.zero;
+
+    [SerializeField]
+    private Camera linkedInspectCam;
+
+    public int index;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && linkedInspectCam.GetComponent<CameraFollow>().objectIndex == index) 
+        {
+            RaycastHit hit;
+
+            Ray ray = linkedInspectCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 10000.0f))
+            {
+                if (hit.collider.gameObject == gameObject) 
+                {
+                    if (hit.collider.gameObject.GetComponent<PreviewObjectFunctionality>().enabled == true)//check that it's an interactable object
+                    {
+                        mPosDelta = Input.mousePosition - mPrevPos;
+
+                        transform.Rotate(Vector3.up, -Vector3.Dot(mPosDelta, linkedInspectCam.transform.right), Space.World); //project left and right mouse movement onto object
+
+                        transform.Rotate(linkedInspectCam.transform.right, Vector3.Dot(mPosDelta, linkedInspectCam.transform.up), Space.World); //project other mouse movement onto object
+                    }
+                }
+            }
+        }
+        mPrevPos = Input.mousePosition;
+    }
+}
