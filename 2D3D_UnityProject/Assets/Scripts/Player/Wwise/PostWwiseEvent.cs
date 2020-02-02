1cf3ae4e8779962455a91c669e7a70c06a3a20e9
julieraptor <42128030+julieraptor@users.noreply.github.com>
2020-02-01 19:02:38 -0500

    Wwise Integration
    - Integrated Wwise
    - Setting looped music
    - Added footsteps (they play endlessly because there is only 1 frame of animation)
    - Trying to add states which change music depending if interacting with object or not

---
+++ 2D3D_UnityProject/Assets/Scripts/Player/Wwise/PostWwiseEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWwiseEvent : MonoBehaviour
+{
+    public AK.Wwise.Event MyEvent;
+    public void PlayFootstepSound()
+    {
+        MyEvent.Post(gameObject);
+    }
+
+}
---
+++ 2D3D_UnityProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID FOOTSTEP_PLAYER = 648916573U;
+        static const AkUniqueID MUSIC_PLAY = 202194903U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace INTERACTING_WITH_SOMETHING
+        {
+            static const AkUniqueID GROUP = 4244533829U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INTERACTING = 3884110025U;
+                static const AkUniqueID NOTINTERACTING = 927376446U;
+            } // namespace STATE
+        } // namespace INTERACTING_WITH_SOMETHING
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
