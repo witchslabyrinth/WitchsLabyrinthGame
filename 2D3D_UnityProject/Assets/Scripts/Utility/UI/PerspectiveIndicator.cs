d50ae3f0746435e14e2fb6870084dc9a2bc5fa95
Drew Graham <dmg9626@gmail.com>
2020-02-04 03:07:55 -0500

    implemented perspective UI functionality

--- 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveIndicator.cs
+++ 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveIndicator.cs
@@ -2,27 +2,49 @@
 using System.Collections;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class PerspectiveIndicator : MonoBehaviour
 {
     /// <summary>
     /// Associated camera perspective
     /// </summary>
-    [SerializeField] private CameraController.CameraViews perspective;
+    public CameraController.CameraViews cameraView;
+
+    /// <summary>
+    /// Perspective indicator alpha value when not selected
+    /// </summary>
+    [SerializeField] 
+    [Range(0,1)] private float disabledAlpha;
+
+
+    [SerializeField] private Image image;
+
+    /// <summary>
+    /// Sprite to show in image
+    /// </summary>
+    [SerializeField] private Sprite sprite;
 
     /// <summary>
     /// Empty rect below image that adds bottom padding when enabled
     /// </summary>
     [SerializeField] private RectTransform bottomOffset;
 
+
     // Use this for initialization
     void Start()
     {
-
+        // Set image sprite
+        image.sprite = sprite;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetSelected(bool selected)
     {
+        // Set image alpha
+        Color color = image.color;
+        color.a = selected ? 1f : disabledAlpha;
+        image.color = color;
 
+        // Enable/disable bottom offset
+        bottomOffset.gameObject.SetActive(selected);
     }
 }
--- 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveUI.cs
+++ 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveUI.cs
@@ -3,19 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class PerspectiveUI : MonoBehaviour
+public class PerspectiveUI : Singleton<PerspectiveUI>
 {
-    /// <summary>
-    /// Perspective indicator alpha value when not selected
-    /// </summary>
-    [SerializeField] 
-    [Range(0,1)] private float disabledAlpha;
-
     private List<PerspectiveIndicator> perspectiveIndicators;
 
     void Start()
     {
         // Get perspective indicators from children
         perspectiveIndicators = GetComponentsInChildren<PerspectiveIndicator>().ToList();
+
+        // Get player actor's perspective and select that one in UI
+        Actor actor = PlayerController.Instance.GetPlayer();
+        SelectPerspective(actor.perspective);
+    }
+
+    public void SelectPerspective(Perspective perspective)
+    {
+        // Select associated UI indicator
+        PerspectiveIndicator selected = perspectiveIndicators.Find(p => p.cameraView.Equals(perspective.cameraView));
+        selected.SetSelected(true);
+
+        // Deselect all others
+        foreach (PerspectiveIndicator perspectiveIndicator in perspectiveIndicators.Where(p => p.cameraView != perspective.cameraView))
+        {
+            perspectiveIndicator.SetSelected(false);
+        }
     }
 }
