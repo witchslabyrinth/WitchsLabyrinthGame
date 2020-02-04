7e25fa91ed79216689c4ce506840ebf772dda687
Drew Graham <dmg9626@gmail.com>
2020-02-04 02:46:22 -0500

    started setting up scripts for perspective indicators

---
+++ 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveIndicator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PerspectiveIndicator : MonoBehaviour
+{
+    /// <summary>
+    /// Associated camera perspective
+    /// </summary>
+    [SerializeField] private CameraController.CameraViews perspective;
+
+    /// <summary>
+    /// Empty rect below image that adds bottom padding when enabled
+    /// </summary>
+    [SerializeField] private RectTransform bottomOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PerspectiveUI : MonoBehaviour
+{
+    /// <summary>
+    /// Perspective indicator alpha value when not selected
+    /// </summary>
+    [SerializeField] 
+    [Range(0,1)] private float disabledAlpha;
+
+    private List<PerspectiveIndicator> perspectiveIndicators;
+
+    void Start()
+    {
+        // Get perspective indicators from children
+        perspectiveIndicators = GetComponentsInChildren<PerspectiveIndicator>().ToList();
+    }
+}
