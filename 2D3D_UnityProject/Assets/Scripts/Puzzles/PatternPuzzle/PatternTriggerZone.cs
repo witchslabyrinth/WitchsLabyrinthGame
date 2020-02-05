35b9e5a6399c7dbda34d54255c0fcd73ea528161
Victor Liu <vsl27@drexel.edu>
2020-02-04 20:51:22 -0500

    Made pattern puzzle a prefab and setup trigger zone in main scene

---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/PatternPuzzle/PatternTriggerZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternTriggerZone : MonoBehaviour
+{
+    [SerializeField]
+    private PatternPuzzle puzzleScript;
+
+    [SerializeField]
+    private GameObject camPattern;
+
+    [Header("Wwise")]
+    /// <summary>
+    /// Set Wwise variables
+    /// </summary>
+    /// <param name="paused">Set Wwise variables for sounds here</param>
+    public AK.Wwise.Event playerReaction; //The reaction sound when nearby this puzzle for Oliver 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInteractionController controller = other.GetComponent<PlayerInteractionController>();
+        if (controller != null)
+            controller.SetInPatternZone(true, puzzleScript, camPattern);
+        Debug.Log("In range of Pattern Puzzle");
+        playerReaction.Post(gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerInteractionController controller = other.GetComponent<PlayerInteractionController>();
+        if (controller != null)
+            controller.SetInPatternZone(false, puzzleScript, camPattern);
+        Debug.Log("Out of range of Pattern Puzzle");
+
+    }
+}
