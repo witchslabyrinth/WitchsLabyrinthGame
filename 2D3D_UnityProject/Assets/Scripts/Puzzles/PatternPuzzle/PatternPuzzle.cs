2ba169d0596bcf4ad7015d944e39743c45cc5a4a
Wills-M <wmm38@drexel.edu>
2020-02-04 19:49:56 -0500

    Pattern puzzle basic functionality in its own scene.

---
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/PatternPuzzle/PatternPuzzle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternPuzzle : MonoBehaviour
+{
+    [SerializeField]
+    private List<PatternCube> patternCubes;
+
+    [SerializeField]
+    [Tooltip("The cube that will start selected when the scene loads. 0 for leftmost cube, 4 for rightmost cube.")]
+    [Range(0, 4)]
+    private int initalCube;
+
+    private int _currentCube;
+    private int CurrentCube
+    {
+        get => _currentCube;
+        set
+        {
+            patternCubes[_currentCube].Deselect();
+            _currentCube = value;
+            patternCubes[_currentCube].Select();
+        }
+    }
+
+    private void Start()
+    {
+        CurrentCube = initalCube;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            patternCubes[CurrentCube].Rotate(Direction.BACKWARD);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CurrentCube = Mathf.Clamp(CurrentCube - 1, 0, 4);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            patternCubes[CurrentCube].Rotate(Direction.FORWARD);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            CurrentCube = Mathf.Clamp(CurrentCube + 1, 0, 4);
+        }
+    }
+
+    public enum Direction
+    {
+        FORWARD = 1,
+        BACKWARD = -1
+    }
+}
