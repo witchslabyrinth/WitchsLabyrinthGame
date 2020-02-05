c8f5192d88c4695332a946e13aa633fbb941af8f
Drew Graham <dmg9626@gmail.com>
2020-02-05 14:04:41 -0500

    UI: added perspective indicators (#81)
    * started wireframing perspective UI
    
    * added a bottom padding for when perspective is selected
    
    * started setting up scripts for perspective indicators
    
    * implemented perspective UI functionality
    
    * made perspective icons smaller
    
    * moved perspective indicators to bottom left; made them smaller
    
    * oops - moved GetComponent<> calls from Start() to Awake() to avoid nullrefs
    
    * added UI elements (pause menu/perspective indicators) to teledoor scene

---
+++ 2D3D_UnityProject/Assets/Scripts/Utility/UI/PerspectiveUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PerspectiveUI : Singleton<PerspectiveUI>
+{
+    private List<PerspectiveIndicator> perspectiveIndicators;
+
+    void Awake()
+    {
+        // Get perspective indicators from children
+        perspectiveIndicators = GetComponentsInChildren<PerspectiveIndicator>().ToList();
+    }
+
+    public void SelectPerspective(Perspective perspective)
+    {
+        // Select associated UI indicator
+        PerspectiveIndicator selected = perspectiveIndicators.Find(p => p.cameraView.Equals(perspective.cameraView));
+        selected.SetSelected(true);
+
+        // Deselect all others
+        foreach (PerspectiveIndicator perspectiveIndicator in perspectiveIndicators.Where(p => p.cameraView != perspective.cameraView))
+        {
+            perspectiveIndicator.SetSelected(false);
+        }
+    }
+}
