ba8c10d2e10264bd6f2df61262ea6fdb5997910e
Zoe <sorafnlform@gmail.com>
2020-02-07 20:02:28 -0500

    Little glitchy, but functional camera pushing

--- 2D3D_UnityProject/Assets/Scripts/PerspectiveCameraControl.cs
+++ 2D3D_UnityProject/Assets/Scripts/PerspectiveCameraControl.cs
@@ -2,68 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PerspectiveCameraControl : MonoBehaviour {
+public class PerspectiveCameraControl : MonoBehaviour
+{
     public Camera Camera { get; private set; }
 
     Vector2 _mouseAbsolute;
     Vector2 _smoothMouse;
 
-    public Vector2 clampInDegrees = new Vector2 (360, 180);
+    public Vector2 clampInDegrees = new Vector2(360, 180);
     //public bool lockCursor;
-    public Vector2 sensitivity = new Vector2 (2, 2);
-    public Vector2 smoothing = new Vector2 (3, 3);
+    public Vector2 sensitivity = new Vector2(2, 2);
+    public Vector2 smoothing = new Vector2(3, 3);
     public Vector2 targetDirection;
     public Vector2 targetCharacterDirection;
+    public float distance = 3f;
 
     public GameObject characterBody;
 
-    void Start () {
+    private Vector3 startLocalPosition;
+    private Vector3 relativePosition;
+
+    void Start()
+    {
         // Set target direction to the camera's initial orientation.
         targetDirection = transform.localRotation.eulerAngles;
+
+        startLocalPosition = transform.localPosition;
+        relativePosition = transform.position - characterBody.transform.position;
 
         // Set target direction for the character body to its inital state.
         if (characterBody)
             targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
     }
 
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
         // Ensure the cursor is always locked when set
         GameManager.SetCursorActive(false);
 
         // Allow the script to clamp based on a desired target value.
-        var targetOrientation = Quaternion.Euler (targetDirection);
-        var targetCharacterOrientation = Quaternion.Euler (targetCharacterDirection);
+        var targetOrientation = Quaternion.Euler(targetDirection);
+        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
 
         // Get raw mouse input for a cleaner reading on more sensitive mice.
-        var mouseDelta = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         // Scale input against the sensitivity setting and multiply that against the smoothing value.
-        mouseDelta = Vector2.Scale (mouseDelta, new Vector2 (sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
 
         // Interpolate mouse movement over time to apply smoothing delta.
-        _smoothMouse.x = Mathf.Lerp (_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
-        _smoothMouse.y = Mathf.Lerp (_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
 
         // Find the absolute mouse movement value from point zero.
         _mouseAbsolute += _smoothMouse;
 
         // Clamp and apply the local x value first, so as not to be affected by world transforms.
         if (clampInDegrees.x < 360)
-            _mouseAbsolute.x = Mathf.Clamp (_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
 
         // Then clamp and apply the global y value.
         if (clampInDegrees.y < 360)
-            _mouseAbsolute.y = Mathf.Clamp (_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+            _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
 
-        transform.localRotation = Quaternion.AngleAxis (-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
+        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
 
         // If there's a character body that acts as a parent to the camera
-        if (characterBody) {
-            var yRotation = Quaternion.AngleAxis (_mouseAbsolute.x, Vector3.up);
+        if (characterBody)
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
             characterBody.transform.localRotation = yRotation * targetCharacterOrientation;
-        } else {
-            var yRotation = Quaternion.AngleAxis (_mouseAbsolute.x, transform.InverseTransformDirection (Vector3.up));
+        }
+        else
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
             transform.localRotation *= yRotation;
+        }
+    }
+
+    void LateUpdate()
+    {
+
+        RaycastHit hit;
+        Debug.DrawRay(characterBody.transform.position, relativePosition, Color.blue);
+        if (Physics.Raycast(characterBody.transform.position, relativePosition, out hit, distance + 0.5f))
+        {
+            transform.position = new Vector3(hit.point.x + hit.normal.x * 0.5f, transform.position.y, hit.point.z + hit.normal.z * 0.5f);
+        }
+        else
+        {
+            transform.localPosition = startLocalPosition;
+            relativePosition = transform.position - characterBody.transform.position;
         }
     }
 }
