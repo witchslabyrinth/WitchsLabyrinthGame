92e24538341fc683b35e611d3bc2a2664661def8
Victor Liu <vsl27@drexel.edu>
2020-02-14 06:00:57 -0500

    adding a command for 3rd camera action

--- 2D3D_UnityProject/Assets/Scripts/DialogueCommands.cs
+++ 2D3D_UnityProject/Assets/Scripts/DialogueCommands.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class DialogueCommands : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Drag and drop your Dialogue Runner into this variable.
+    [SerializeField]
+    private DialogueRunner dialogueRunner;
+
+    [SerializeField]
+    private GameObject camera3;
+
+    [SerializeField]
+    private float angle = 10f;
+
+    public void Awake() 
     {
-        
+        dialogueRunner.AddCommandHandler(
+            "op_camera_3",
+            OpCamera3
+        );
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// third camera action, map pan
+    /// </summary>
+    /// <param name="parameters"></param>
+    /// <param name="onComplete"></param>
+    private void OpCamera3(string[] parameters)
     {
-        
+        camera3.SetActive(true);
+        StartCoroutine(wholeMapPan(camera3));
+    }
+
+    /// <summary>
+    /// pans whole map
+    /// </summary>
+    /// <param name="camera"></param>
+    /// <returns></returns>
+    private IEnumerator wholeMapPan(GameObject camera)
+    {
+        while(true)
+        {
+            camera.transform.RotateAround(transform.position, Vector3.up, angle*Time.deltaTime);
+            yield return null;
+        }
     }
 }
