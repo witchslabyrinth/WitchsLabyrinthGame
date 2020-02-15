09e8d8f5b76d5b9c176f38ac133d8ec6eaef80d5
Victor Liu <vsl27@drexel.edu>
2020-02-15 03:24:50 -0500

    First camera action

--- 2D3D_UnityProject/Assets/Scripts/DialogueCommands.cs
+++ 2D3D_UnityProject/Assets/Scripts/DialogueCommands.cs
@@ -13,19 +13,31 @@ public class DialogueCommands : MonoBehaviour
     private GameObject opCamera;
 
     [SerializeField]
+    private Transform cameraLoc1;
+
+    [SerializeField]
     private Transform cameraLoc3;
+
+    [SerializeField]
+    private Transform cameraLoc3Center;
 
     [SerializeField]
     private Transform cameraLoc4;
 
     [SerializeField]
-    private float panToTempleTime = 3f;
+    private float panToCatTime = 2f;
+
+    [SerializeField]
+    private float panToTempleTime = 2f;
 
     [SerializeField]
     private float angle = 10f;
 
+    private Coroutine currCoroutine;
+
     public void Awake() 
     {
+        dialogueRunner.AddCommandHandler("op_camera_1", OpCamera1);
         dialogueRunner.AddCommandHandler("op_camera_3", OpCamera3);
         dialogueRunner.AddCommandHandler("op_camera_4", OpCamera4);
         dialogueRunner.AddCommandHandler("reset_input", ResetInput);
@@ -33,39 +45,65 @@ public class DialogueCommands : MonoBehaviour
 
     private void ResetInput(string[] parameters, System.Action onComplete)
     {
-        Actor actor = PlayerController.Instance.GetPlayer();
-        actor.ghostCamera.gameObject.SetActive(true);
-        actor.ghostCamera.enabled = true;
-        actor.enabled = true;
+        Actor oliver = PlayerController.Instance.GetPlayer();
+        oliver.ghostCamera.gameObject.SetActive(true);
+        oliver.ghostCamera.enabled = true;
+        oliver.enabled = true;
+        
+        Actor cat = PlayerController.Instance.GetFriend();
+        cat.enabled = true;
+        cat.GetComponentInChildren<AnimationController>().enabled = true;
+
         GameManager.SetCursorActive(false);
-        ResetCoroutines();
+        StopCoroutine(currCoroutine);
         onComplete();
+    }
+
+    private void OpCamera1(string[] parameters, System.Action onComplete)
+    {
+        opCamera.transform.position = PlayerController.Instance.GetPlayer().ghostCamera.transform.position;
+        opCamera.transform.rotation = PlayerController.Instance.GetPlayer().ghostCamera.transform.rotation;
+        opCamera.SetActive(true);
+
+        PlayerController.Instance.GetPlayer().ghostCamera.gameObject.SetActive(false);
+        currCoroutine = StartCoroutine(PanToCat(onComplete));
     }
 
     private void OpCamera3(string[] parameters)
     {
-        opCamera.SetActive(true);
-        PlayerController.Instance.GetPlayer().ghostCamera.gameObject.SetActive(false);
-        ResetCoroutines();
-        StartCoroutine("WholeMapPan");
+        StopCoroutine(currCoroutine);
+        opCamera.transform.position = cameraLoc3.position;
+        opCamera.transform.rotation = cameraLoc3.rotation;
+        currCoroutine = StartCoroutine(WholeMapPan());
     }
 
     private void OpCamera4(string[] parameters)
     {
-        ResetCoroutines();
-        StartCoroutine("PanToTemple");
+        StopCoroutine(currCoroutine);
+        currCoroutine = StartCoroutine(PanToTemple());
     }
 
-    /// <summary>
-    /// pans whole map
-    /// </summary>
-    /// <param name="camera"></param>
-    /// <returns></returns>
+    private IEnumerator PanToCat(System.Action onComplete)
+    {
+        Vector3 startPos = opCamera.transform.position;
+        Quaternion startRot = opCamera.transform.rotation;
+        for(float time = 0; time < panToCatTime; time += Time.deltaTime)
+        {
+            float percentage = time / panToCatTime;
+            opCamera.transform.position = Vector3.Lerp(startPos, cameraLoc1.position, percentage);
+            opCamera.transform.rotation = Quaternion.Lerp(startRot, cameraLoc1.rotation, percentage);
+            yield return null;
+        }
+        opCamera.transform.position = cameraLoc1.position;
+        opCamera.transform.rotation = cameraLoc1.rotation;
+        onComplete();
+    }
+
     private IEnumerator WholeMapPan()
     {
         while(true)
         {
-            opCamera.transform.RotateAround(cameraLoc3.position, Vector3.up, angle*Time.deltaTime);
+            opCamera.transform.RotateAround(cameraLoc3Center.position, Vector3.up, angle*Time.deltaTime);
             yield return null;
         }
     }
@@ -84,11 +122,5 @@ public class DialogueCommands : MonoBehaviour
 
         opCamera.transform.position = cameraLoc4.position;
         opCamera.transform.rotation = cameraLoc4.rotation;
-    }
-
-    private void ResetCoroutines()
-    {
-        StopCoroutine("WholeMapPan");
-        StopCoroutine("PanToTemple");
     }
 }
