4c83b22c3da03f8a8afd305cb6c6fb62c76d92e3
Victor Liu <vsl27@drexel.edu>
2020-02-14 21:54:28 -0500

    code for 3rd and 4th camera actions

--- 2D3D_UnityProject/Assets/Scripts/DialogueCommands.cs
+++ 2D3D_UnityProject/Assets/Scripts/DialogueCommands.cs
@@ -10,7 +10,16 @@ public class DialogueCommands : MonoBehaviour
     private DialogueRunner dialogueRunner;
 
     [SerializeField]
-    private GameObject camera3;
+    private GameObject opCamera;
+
+    [SerializeField]
+    private Transform cameraLoc3;
+
+    [SerializeField]
+    private Transform cameraLoc4;
+
+    [SerializeField]
+    private float panToTempleTime = 3f;
 
     [SerializeField]
     private float angle = 10f;
@@ -18,27 +27,33 @@ public class DialogueCommands : MonoBehaviour
     public void Awake() 
     {
         dialogueRunner.AddCommandHandler("op_camera_3", OpCamera3);
+        dialogueRunner.AddCommandHandler("op_camera_4", OpCamera4);
         dialogueRunner.AddCommandHandler("reset_input", ResetInput);
     }
 
     private void ResetInput(string[] parameters, System.Action onComplete)
     {
         Actor actor = PlayerController.Instance.GetPlayer();
+        actor.ghostCamera.gameObject.SetActive(true);
         actor.ghostCamera.enabled = true;
         actor.enabled = true;
         GameManager.SetCursorActive(false);
+        ResetCoroutines();
         onComplete();
     }
 
-    /// <summary>
-    /// third camera action, map pan
-    /// </summary>
-    /// <param name="parameters"></param>
-    /// <param name="onComplete"></param>
     private void OpCamera3(string[] parameters)
     {
-        camera3.SetActive(true);
-        StartCoroutine(wholeMapPan(camera3));
+        opCamera.SetActive(true);
+        PlayerController.Instance.GetPlayer().ghostCamera.gameObject.SetActive(false);
+        ResetCoroutines();
+        StartCoroutine("WholeMapPan");
+    }
+
+    private void OpCamera4(string[] parameters)
+    {
+        ResetCoroutines();
+        StartCoroutine("PanToTemple");
     }
 
     /// <summary>
@@ -46,12 +61,34 @@ public class DialogueCommands : MonoBehaviour
     /// </summary>
     /// <param name="camera"></param>
     /// <returns></returns>
-    private IEnumerator wholeMapPan(GameObject camera)
+    private IEnumerator WholeMapPan()
     {
         while(true)
         {
-            camera.transform.RotateAround(transform.position, Vector3.up, angle*Time.deltaTime);
+            opCamera.transform.RotateAround(cameraLoc3.position, Vector3.up, angle*Time.deltaTime);
             yield return null;
         }
+    }
+
+    private IEnumerator PanToTemple()
+    {
+        Vector3 startPos = opCamera.transform.position;
+        Quaternion startRot = opCamera.transform.rotation;
+        for(float time = 0; time < panToTempleTime; time += Time.deltaTime)
+        {
+            float percentage = time / panToTempleTime;
+            opCamera.transform.position = Vector3.Lerp(startPos, cameraLoc4.position, percentage);
+            opCamera.transform.rotation = Quaternion.Lerp(startRot, cameraLoc4.rotation, percentage);
+            yield return null;
+        }
+
+        opCamera.transform.position = cameraLoc4.position;
+        opCamera.transform.rotation = cameraLoc4.rotation;
+    }
+
+    private void ResetCoroutines()
+    {
+        StopCoroutine("WholeMapPan");
+        StopCoroutine("PanToTemple");
     }
 }
