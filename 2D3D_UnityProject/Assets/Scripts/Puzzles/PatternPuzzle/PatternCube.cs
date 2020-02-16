1a527bb7b589d8aa88147d21fdd9b7334820f86b
Wills Martin <wmm38@drexel.edu>
2020-02-16 13:23:17 -0500

    Pattern puzzle updates (#115)
    * Fixed order of pattern puzzle cube order.
    
    * Cubes can now be moved left and right.
    
    * Added win condition and fixed some bugs.
    
    * Rotation of cubes is locally based instead of based on world directions.
    
    * Pattern Textures
    
    New materials for blocks, changed camera angle, and messed with the speed of movement
    
    * Added win animation that plays when you solve the puzzle.
    
    Co-authored-by: maggiejmay <43794946+maggiejmay@users.noreply.github.com>

--- 2D3D_UnityProject/Assets/Scripts/Puzzles/PatternPuzzle/PatternCube.cs
+++ 2D3D_UnityProject/Assets/Scripts/Puzzles/PatternPuzzle/PatternCube.cs
@@ -9,18 +9,91 @@ public class PatternCube : MonoBehaviour
     private float selectYOffset;
 
     [SerializeField]
+    [Tooltip("The amount of time it takes to raise and lower the cube.")]
     private float raiseLowerTime;
 
     [SerializeField]
+    [Tooltip("The amount of time it takes to rotate the cube.")]
     private float rotateTime;
+
+    [SerializeField]
+    [Tooltip("The amount of time it takes to translate the cube.")]
+    private float translateTime;
+
+    [SerializeField]
+    [Tooltip("The correct position of this cube for the solved puzzle. 0 for leftmost, 4 for rightmost.")]
+    private int correctPosition;
+
+    [SerializeField]
+    [Tooltip("The correct rotation of the cube for the solved puzzle. The correct rotation is when the drawn debug ray is pointed upwards (So when the selected local direction matches world up.")]
+    private Rotation correctRotation;
+
+    [HideInInspector]
+    public bool animating;
 
     private const float RotationAmount = 90f;
 
-    private Vector3 startPosition;
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        switch (correctRotation)
+        {
+            case Rotation.Forward:
+                Ray forwardRay = new Ray(transform.position, gameObject.transform.forward);
+                Gizmos.DrawRay(forwardRay);
+                break;
+            case Rotation.Back:
+                Ray backRay = new Ray(transform.position, gameObject.transform.forward * -1f);
+                Gizmos.DrawRay(backRay);
+                break;
+            case Rotation.Up:
+                Ray upRay = new Ray(transform.position, gameObject.transform.up);
+                Gizmos.DrawRay(upRay);
+                break;
+            case Rotation.Down:
+                Ray downRay = new Ray(transform.position, gameObject.transform.up * -1f);
+                Gizmos.DrawRay(downRay);
+                break;
+        }
+    }
+    #endif
 
     private void Awake()
     {
-        startPosition = transform.localPosition;
+        animating = false;
+    }
+
+    public bool Correct(int position)
+    {
+        if (position == correctPosition)
+        {
+            Vector3 direction = Vector3.left;
+
+            switch (correctRotation)
+            {
+                case Rotation.Forward:
+                    direction = gameObject.transform.forward;
+                    break;
+                case Rotation.Back:
+                    direction = gameObject.transform.forward * -1f;
+                    break;
+                case Rotation.Up:
+                    direction = gameObject.transform.up;
+                    break;
+                case Rotation.Down:
+                    direction = gameObject.transform.up * -1f;
+                    break;
+            }
+
+            if (direction == Vector3.up)
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void Select()
@@ -45,10 +118,13 @@ public class PatternCube : MonoBehaviour
     /// <returns></returns>
     private IEnumerator RaiseCoroutine(bool raise)
     {
+        animating = true;
+
         // Get the inital position and the final position of the raise/lower based on the parameter
-        Vector3 raisePosition = new Vector3(startPosition.x, startPosition.y + selectYOffset, startPosition.y);
+        Vector3 raisePosition = new Vector3(transform.localPosition.x, transform.localPosition.y + selectYOffset, transform.localPosition.z);
+        Vector3 lowerPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - selectYOffset, transform.localPosition.z);
         Vector3 initPosition = transform.localPosition;
-        Vector3 finalPosition = raise ? raisePosition : startPosition;
+        Vector3 finalPosition = raise ? raisePosition : lowerPosition;
 
         // Animate from initPosition to finalPosition
         for (float time = 0f; time < raiseLowerTime; time += Time.deltaTime)
@@ -61,6 +137,8 @@ public class PatternCube : MonoBehaviour
 
         // Set final location on last frame of coroutine
         transform.localPosition = finalPosition;
+
+        animating = false;
     }
 
     /// <summary>
@@ -79,6 +157,8 @@ public class PatternCube : MonoBehaviour
     private IEnumerator rotateCoroutineInstance;
     private IEnumerator RotateCoroutine(PatternPuzzle.Direction direction)
     {
+        animating = true;
+
         // Get the initial rotation and calculate final rotation based on parameter
         Quaternion initRotation = transform.localRotation;
         Quaternion rotationAmount = DirectionToQuaternion(direction);
@@ -98,6 +178,66 @@ public class PatternCube : MonoBehaviour
 
         // Set coroutine instance to null so it can run again
         rotateCoroutineInstance = null;
+
+        animating = false;
+    }
+
+    /// <summary>
+    /// Translates cube from initPosition to targetPosition.
+    /// </summary>
+    /// <param name="initPosition"></param>
+    /// <param name="targetPosition"></param>
+    public void Translate(Vector3 initPosition, Vector3 targetPosition)
+    {
+        if (translateCoroutineInstance == null)
+        {
+            translateCoroutineInstance = (translateCoroutine(initPosition, targetPosition));
+            StartCoroutine(translateCoroutineInstance);
+        }
+    }
+
+    private IEnumerator translateCoroutineInstance;
+    private IEnumerator translateCoroutine(Vector3 initPosition, Vector3 targetPosition)
+    {
+        animating = true;
+
+        // Animate from initPosition to targetPosition
+        for (float time = 0f; time < translateTime; time += Time.deltaTime)
+        {
+            Vector3 newPosition = Vector3.Lerp(initPosition, targetPosition, time / translateTime);
+            transform.localPosition = newPosition;
+
+            yield return null;
+        }
+
+        // Set final position on last frame of coroutine
+        transform.localPosition = targetPosition;
+
+        // Set coroutine instance to null so it can run again
+        translateCoroutineInstance = null;
+
+        animating = false;
+    }
+
+    public void Jump(AnimationCurve animationCurve, float jumpTime)
+    {
+        StartCoroutine(JumpCoroutine(animationCurve, jumpTime));
+    }
+
+    private IEnumerator JumpCoroutine(AnimationCurve animationCurve, float jumpTime)
+    {
+        Vector3 startPosition = transform.position;
+
+        for (float time = 0f; time < 1f; time += Time.deltaTime)
+        {
+            float newY = animationCurve.Evaluate(time / jumpTime);
+            Vector3 newPosition = new Vector3(startPosition.x, startPosition.y + newY, startPosition.z);
+            transform.position = newPosition;
+
+            yield return null;
+        }
+
+        transform.position = startPosition;
     }
 
     /// <summary>
@@ -110,10 +250,18 @@ public class PatternCube : MonoBehaviour
         switch(direction)
         {
             case PatternPuzzle.Direction.BACKWARD:
-                return Quaternion.AngleAxis(RotationAmount, Vector3.left);
+                return Quaternion.AngleAxis(RotationAmount, gameObject.transform.right);
             case PatternPuzzle.Direction.FORWARD:
-                return Quaternion.AngleAxis(RotationAmount, Vector3.right);
+                return Quaternion.AngleAxis(RotationAmount, gameObject.transform.right * -1f);
         }
         throw new System.Exception("Unexpected code path reached.");
+    }
+
+    private enum Rotation
+    {
+        Forward,
+        Back,
+        Up,
+        Down
     }
 }
