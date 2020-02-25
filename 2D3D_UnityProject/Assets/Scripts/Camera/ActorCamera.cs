bb24c7124baa66101915f71789193f1074ad9539
Drew Graham <dmg9626@gmail.com>
2020-02-25 02:21:27 -0500

    implemented first pass of smooth camera rotation

--- 2D3D_UnityProject/Assets/Scripts/Camera/ActorCamera.cs
+++ 2D3D_UnityProject/Assets/Scripts/Camera/ActorCamera.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ActorCamera : CameraEntity
 {
-    protected Vector3 rightRotation = new Vector3 (0f, -90f, 0f);
-    protected Vector3 topRotation = new Vector3 (90f, 0f, 0f);
-    protected Vector3 backRotation = new Vector3 (0f, 0f, 0f);
-
-    public float orthoOffset = 2.0f;
-    public float headway = 2.0f;
-
     public enum CameraViews {
         RIGHT,
         TOP,
@@ -25,6 +19,16 @@ public class ActorCamera : CameraEntity
     /// </summary>
     [SerializeField]
     private Actor actor;
+
+    /// <summary>
+    /// Speed of camera transitions between perspectives
+    /// </summary>
+    [SerializeField]
+    [Range(0, 1.5f)]
+    private float perspectiveTransitionSpeed;
+
+    public Vector3 lastThirdPersonCameraPosition;
+    public Vector3 lastThirdPersonCameraRotation;
 
     void Start () 
     {
@@ -44,8 +48,11 @@ public class ActorCamera : CameraEntity
     /// Updates camera position/rotation relative to provided (player-controlled) Actor
     /// </summary>
     /// <param name="player">Actor currently controlled by player</param>
-    public override void CameraUpdate () 
+    public override void CameraUpdate ()
     {
+        if (transitionPerspectiveCoroutine != null)
+            return;
+
         if(perspective.orthographic)
             OrthographicUpdate();
         else 
@@ -55,23 +62,60 @@ public class ActorCamera : CameraEntity
     /// <summary>
     /// Initializes camera to follow specified Actor with specified Perspective.
     /// </summary>
-    /// <param name="actor">Actor to follow</param>
     /// <param name="perspective">New camera perspective</param>
     public void SetPerspective(Perspective perspective)
     {
         // Check if we're changing to a new perspective
         // TODO: see if this is a good conditional to do a smooth perspective transition on
-        if(perspective != this.perspective)
-            Debug.LogFormat("Setting new perspective ({0}) on actor {1}", perspective.cameraView.ToString(), actor.name);
+        if (perspective == this.perspective || transitionPerspectiveCoroutine != null)
+            return;
+        Debug.LogFormat("Setting new perspective ({0}) on actor {1}", perspective.cameraView.ToString(), actor.name);
 
-        // Save current perspective
-        this.perspective = perspective;
-        
-        // Set fixed camera rotation for orthographic
-        if(perspective.orthographic) {
-            transform.eulerAngles = perspective.orthographicCameraRotation;
+        if (this.perspective != null)
+            transitionPerspectiveCoroutine = StartCoroutine(TransitionPerspective(this.perspective, perspective));
+    }
+
+    private Coroutine transitionPerspectiveCoroutine;
+    private IEnumerator TransitionPerspective(Perspective from, Perspective to)
+    {
+        // Update orthographic setting before moving camera, if we're not currently orthographic
+        camera.orthographic = to.orthographic;
+
+        // Disable swapping until transition complete
+        PlayerController.Instance.canSwap = false;
+
+        // Lerp from camera's starting transfrom values
+        Vector3 startPosition = transform.position;
+        Vector3 startRotation = transform.eulerAngles;
+
+        Vector3 endPosition = to.orthographic ? to.orthographicCameraOffset : lastThirdPersonCameraPosition;
+        Vector3 endRotation = to.orthographic ? to.orthographicCameraRotation: lastThirdPersonCameraRotation;
+        Vector3 endPositionOffset = Vector3.zero;
+
+        float t = 0;
+        while(t < 1) {
+            // Only apply fixed position offset in orthographic perspectives
+            endPositionOffset = to.orthographic ? actor.transform.position : Vector3.zero;
+
+            // Update position/rotation values
+            transform.position =  Mathfx.Sinerp(startPosition, endPosition + endPositionOffset, t);
+            transform.eulerAngles =  Mathfx.Sinerp(startRotation, endRotation, t);
+
+            float step = 1 / perspectiveTransitionSpeed * Time.deltaTime;
+            t += step;
+            yield return null;
         }
-        camera.orthographic = perspective.orthographic;
+
+        // Make sure we hit the target
+        transform.position = endPosition + endPositionOffset;
+        transform.eulerAngles = endRotation;
+
+        // Update camera perspective
+        perspective = to;
+
+        // Restore swapping
+        PlayerController.Instance.canSwap = true;
+        transitionPerspectiveCoroutine = null;
     }
 
     /// <summary>
@@ -83,8 +127,11 @@ public class ActorCamera : CameraEntity
         // Update ghost camrea orientation
         actor.ghostCamera.CameraUpdate();
 
-        this.transform.position = actor.ghostCamera.transform.position;
-        this.transform.rotation = actor.ghostCamera.transform.rotation;
+        transform.rotation = actor.ghostCamera.transform.rotation;
+        transform.position = actor.ghostCamera.transform.position;
+
+        lastThirdPersonCameraPosition = actor.ghostCamera.transform.position;
+        lastThirdPersonCameraRotation = new Vector3(transform.eulerAngles.x, actor.transform.eulerAngles.y, 0);
     }
 
     /// <summary>
