4bc4db41d1eda9000e21fe21133d07f7642d67d9
Victor Liu <vsl27@drexel.edu>
2020-02-27 18:11:37 -0500

    Scene 8 hooked up

---
+++ 2D3D_UnityProject/Assets/Scripts/Yarnspinner Commands/Scene8Trigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Scene8Trigger : MonoBehaviour
+{
+    [SerializeField]
+    private YarnProgram scene8;
+
+    [SerializeField]
+    private CameraEntity scene8Cam;
+
+    private bool scenePlayed;
+
+    void Start()
+    {
+        if (scene8 != null)
+        {
+            DialogueRunner dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+            dialogueRunner.Add(scene8);
+        }
+        scenePlayed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!scenePlayed)
+        {
+            FindObjectOfType<DialogueRunner>().StartDialogue("AfterPortalPuzzle");
+            CameraController.Instance.SetMainCamera(scene8Cam);
+        }
+    }
+}
