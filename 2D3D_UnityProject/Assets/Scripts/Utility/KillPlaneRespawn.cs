17b966f99b9f2743af5a8ba7eff3048fa0cf119d
n8sthegr8est <naa73@drexel.edu>
2020-03-10 13:47:06 -0400

    Added Kill Plane to Ukiyo-e Scene, Added KillPlaneRespawn to Oliver and Cat

---
+++ 2D3D_UnityProject/Assets/Scripts/Utility/KillPlaneRespawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlaneRespawn : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    public void respawn()
+    {
+        transform.position = initialPosition;
+    }
+}
---
+++ 2D3D_UnityProject/Assets/Scripts/Utility/KillPlaneScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlaneScript : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 defaultRespawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        KillPlaneRespawn killPlaneRespawn = other.GetComponent<KillPlaneRespawn>();
+        if(killPlaneRespawn == null)
+        {
+            other.transform.position = defaultRespawnPosition;
+        }
+        else
+        {
+            killPlaneRespawn.respawn();
+        }
+    }
+}
