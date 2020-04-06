479c23a51cca7ae0e73f5a47e4a212ca151ef193
Drew Graham <dmg9626@gmail.com>
2020-04-06 17:19:11 -0400

    CameraTrigger now supports actor swapping

--- 2D3D_UnityProject/Assets/Scripts/Camera/CameraTrigger.cs
+++ 2D3D_UnityProject/Assets/Scripts/Camera/CameraTrigger.cs
@@ -19,7 +19,14 @@ public class CameraTrigger : MonoBehaviour
     /// </summary>
     [SerializeField]
     private OldCameraController.CameraViews cameraView;
+
+    /// <summary>
+    /// Used to track actors within trigger bounds
+    /// </summary>
+    private List<Actor> actorsInTrigger = new List<Actor>();
+
     private PerspectiveController perspectiveController => PerspectiveController.Instance;
+    private CameraController cameraController => CameraController.Instance;
 
     void Awake()
     {
@@ -32,35 +39,64 @@ public class CameraTrigger : MonoBehaviour
             Debug.LogError($"Error: CameraTrigger {name} | collider not marked as Trigger");
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void LateUpdate()
     {
-        // Ignore everything but the player actor
-        Actor player = PlayerController.Instance.GetPlayer();
-        if (other.gameObject != player.gameObject)
+        if (actorsInTrigger.Count == 0)
             return;
 
-        // Set player movement/animation to match this camera's pespective
-        perspectiveController.SetPerspective(player, perspectiveController.GetPerspectiveByType(cameraView));
-        //player.SetTopView(cameraView == OldCameraController.CameraViews.TOP);
+        // If player is in trigger zone, make sure we're using the referenced camera (handles actor swapping)
+        Actor player = PlayerController.Instance.GetPlayer();
+        if (actorsInTrigger.Contains(player) && cameraController.GetMainCamera() == player.actorCamera) {
+            SwitchToCamera(player);
+        }
+    }
 
-        // Switch to specified camera
-        Debug.LogWarning($"Switching to trigger camera: {camera.name}");
-        CameraController.Instance.SetMainCamera(camera);
+    private void OnTriggerEnter(Collider other)
+    {
+        // Ignore interactions with non-actor objects
+        if (!other.TryGetComponent(out Actor actor))
+            return;
+
+        // Track actor in bounds
+        actorsInTrigger.Add(actor);
+
+        // If this is the player, switch to the referenced camera
+        if (actor == PlayerController.Instance.GetPlayer())
+        {
+            SwitchToCamera(actor);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // Ignore everything but the player actor
-        Actor player = PlayerController.Instance.GetPlayer();
-        if (other.gameObject != player.gameObject)
+        // Ignore interactions with non-actor objects
+        if (!other.TryGetComponent(out Actor actor))
             return;
 
-        // Switch to actor camera
-        Debug.LogWarning($"Switching to actor camera ({player.name})");
-        CameraController.Instance.SetMainCamera(player.actorCamera);
+        // Stop tracking this actor
+        actorsInTrigger.Remove(actor);
 
-        // Set actor to 3rd person perspective
-        Perspective thirdPersonPerspective = perspectiveController.GetPerspectiveByType(OldCameraController.CameraViews.THIRD_PERSON);
-        perspectiveController.SetPerspective(player, thirdPersonPerspective);
+        // If this is the player, switch back to actor camera
+        if (actor == PlayerController.Instance.GetPlayer())
+        {
+            cameraController.SetMainCamera(actor.actorCamera);
+
+            // Set actor to 3rd person perspective
+            Perspective thirdPersonPerspective = perspectiveController.GetPerspectiveByType(OldCameraController.CameraViews.THIRD_PERSON);
+            perspectiveController.SetPerspective(actor, thirdPersonPerspective);
+        }
+    }
+
+    /// <summary>
+    /// Switches to referenced camera, updating actor movement/animation accordingly
+    /// </summary>
+    /// <param name="actor"></param>
+    private void SwitchToCamera(Actor actor)
+    {
+        // Set player movement/animation to match this camera's pespective
+        perspectiveController.SetPerspective(actor, perspectiveController.GetPerspectiveByType(cameraView));
+
+        // Switch to specified camera
+        cameraController.SetMainCamera(camera);
     }
 }
