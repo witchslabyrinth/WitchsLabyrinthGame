3c9e0a3eafca7d6b1926829724ee6df6bd9b5084
Drew Graham <dmg9626@gmail.com>
2020-05-14 16:20:20 -0400

    river bridge only appears after puzzle is solved

---
+++ 2D3D_UnityProject/Assets/Scripts/Utility/RiverBridge.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverBridge : MonoBehaviour
+{
+    private KoiFishPuzzle koiFishPuzzle => KoiFishPuzzle.Instance;
+
+    /// <summary>
+    /// Bridges allowing player to cross river
+    /// </summary>
+    [SerializeField]
+    private GameObject bridges;
+
+    /// <summary>
+    /// Colliders blocking player from crossing river before bridges appear
+    /// </summary>
+    [SerializeField]
+    private GameObject blockingColliders;
+
+    private void Start()
+    {
+        // Unlock bridge when puzzle is solved
+        koiFishPuzzle.onSolved += () => SetRiverPassage(true);
+
+        // Block river passage until player solves puzzle
+        SetRiverPassage(false);
+    }
+
+    private void SetRiverPassage(bool open)
+    {
+        // Allow/block river passage
+        bridges.SetActive(open);
+        blockingColliders.SetActive(!open);
+    }
+}
